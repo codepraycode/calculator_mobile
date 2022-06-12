18aa151fb3506944e2e54f516b296aa2a43e672f
Precious Olusola <preciousolusola16@gmail.com>
2022-06-12 23:03:20 +0100

    working on calculator, setup layout

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from './assets/styles';
+
+
+const Button = ()=>{
+  return (
+    <View style={screen_styles.button}>
+      <Text>1</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={screen_styles.container}>
+      <View style={screen_styles.header}>
+        <View>
+          <Text style={{fontSize:30}}>CALC</Text>
+        </View>
+
+        <View style={screen_styles.toogle}/>
+
+      </View>
+
+
+      <View style={screen_styles.screen}/>
+
+      <View style={screen_styles.controls}>
+        <View style={screen_styles.buttons}>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+        </View>
+
+        <View style={screen_styles.buttons}>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+        </View>
+
+        <View style={screen_styles.buttons}>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+        </View>
+
+
+        <View style={[screen_styles.buttons, screen_styles.last]}>
+          <Button/>
+          <Button/>
+          
+        </View>
+
+
+        
+      </View>
+
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const screen_styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: styles.lightGray,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  header:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+    width:'80%'
+  },
+  toogle:{
+    width:30,
+    height:30,
+    backgroundColor:styles.grayishRed,
+  },
+  screen:{
+    width:'80%',
+    height:100,
+    backgroundColor:styles.veryLightGrey,
+  },
+  controls:{
+    backgroundColor:styles.grayishRed,
+    // paddingVertical:20,
+    width:'80%',
+    // flexDirection:'row'
+  },
+  last:{
+    paddingBottom:20,
+  },
+  buttons:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+    width:'100%',
+    paddingTop:20,
+  },
+  
+  button:{
+    width:50,
+    height:50,
+    backgroundColor:styles.grayishYellow,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'center',
+    marginHorizontal:10,
+    flex:1,
+  }
 });
---
+++ assets/styles.js
@@ -0,0 +1,19 @@
+export default {
+    //  Background
+    lightGray: "hsl(0, 0%, 90%)",
+    grayishRed: "hsl(0, 5%, 81%)",
+    veryLightGrey: "hsl(0, 0%, 93%)",
+
+
+    // Keys
+    darkCyan: "hsl(185, 42%, 37%)",
+    veryDarkCyan: "hsl(185, 58%, 25%)",
+    orange: "hsl(25, 98%, 40%)",
+    darkOrange: "hsl(25, 99%, 27%)",
+    grayishYellow: "hsl(45, 7%, 89%)",
+    darkGrayishYellow: "hsl(35, 11%, 61%)",
+
+    // Text
+    veryDarkGrayishYellow: "hsl(60, 10%, 19%)",
+    white: "hsl(0, 0, 100%)",
+}
